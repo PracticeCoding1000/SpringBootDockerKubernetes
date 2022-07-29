907a2f2fd07b0ec2b0325061716efa4175013a27
roopa <mbagi.roopa@gmail.com>
2022-07-29 19:52:47 +0530

    initial commit

---
+++ src/test/java/com/dockerint/springBootDockerKubernaties/SpringBootDockerKubernatiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerint.springBootDockerKubernaties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerKubernatiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
